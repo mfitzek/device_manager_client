9f708c04fb25b7cde16ade561025ab219fa879bb
mfitzek <m4rfitzek@gmail.com>
2022-03-03 12:52:01 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Init</h1>
+  <Login></Login>
+
+</template>
+
+
+<script setup lang="ts">
+  import Login from "@/components/auth/Login.vue"
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
