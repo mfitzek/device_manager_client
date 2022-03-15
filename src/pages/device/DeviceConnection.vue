3711c899cea18dc4fad1144ef51ea8fc8f6331ea
mfitzek <m4rfitzek@gmail.com>
2022-03-15 13:54:57 +0100

    device connection ui desgin

--- src/pages/device/DeviceConnection.vue
+++ src/pages/device/DeviceConnection.vue
@@ -1,17 +1,30 @@
 <template>
     <div>
-        Connection
+        <q-select v-model="type" :options="options" label="Connection type" outlined />
+        <q-input v-model="con_string" type="text" label="Connection string" />
+
+
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
     setup () {
         
+        const type = ref(null);
+        const con_string = ref(null);
 
-        return {}
+        const options = [
+            {label: "MQTT", value: "mqtt"},
+            {label: "HTTP", value: "http"},
+            {label: "CoAP", value: "coap"},
+        ]
+
+        
+
+        return {type, con_string, options}
     }
 })
 </script>
