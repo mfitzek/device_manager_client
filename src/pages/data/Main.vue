9c008bd6fa9656fd2b3a82df094f0cad896dcc17
mfitzek <m4rfitzek@gmail.com>
2022-04-07 23:37:38 +0200

    Graph

--- src/pages/data/Main.vue
+++ src/pages/data/Main.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="row">
         <div class="col-3 q-pa-sm">
-            <q-scroll-area style="height: 90vh">
+            <q-input label="Search" outlined class="q-py-md" type="search" v-model="search">
+                <template v-slot:append>
+                    <q-icon name="search" search />
+                </template>
+            </q-input>
+            <q-scroll-area style="height: 80vh">
                 <q-list bordered separator>
-                    <q-expansion-item v-for="dev in devices" :label="dev.name" default-opened>
+                    <q-expansion-item v-for="dev in filtered_attributes" :label="dev.name" default-opened>
                         <q-list>
                             <q-item v-for="attr in dev.attributes" clickable @click="click_attr(attr.id!)" :active="is_active(attr.id!)">
                                 <q-item-section>{{ attr.name }}</q-item-section>
@@ -16,14 +21,17 @@
         </div>
         <div class="col-9 q-pa-sm">
             <div class="row">
-                <div class="col-auto">
+                <div class="col-3 q-pa-md">
+                    <q-select v-model="range" label="Show data" :options="['Last day', 'Last week', 'Last month']" filled></q-select>
+                </div>
+                <div class="col-3">
                     <DateTimePicker v-model="date_start" label="Date Start"></DateTimePicker>
                 </div>
-                <div class="col-auto">
+                <div class="col-3">
                     <DateTimePicker v-model="date_end" label="Date End"></DateTimePicker>
                 </div>
-                <div class="col-auto">
-                    <q-btn @click="fetch_data()">Refresh</q-btn>
+                <div class="col-3 q-pa-md">
+                    <q-btn color="primary" @click="fetch_data()" icon-right="refresh">Refresh</q-btn>
                 </div>
             </div>
             <Line :chart-data="chartData" :chart-options="options" :height="200" />
@@ -32,7 +40,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onBeforeMount, ref } from "vue";
+import { defineComponent, onBeforeMount, ref, watch } from "vue";
 import DateTimePicker from "@/components/common/DateTimePicker.vue";
 
 import { Line } from "vue-chartjs";
@@ -43,9 +51,11 @@ ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, PointElement,
 import { ChartData, ChartOptions } from "chart.js";
 
 import { GetDevicesAttributes, FetchTelemetry } from "@store/device";
-import { IDeviceAttributes } from "@/types/device";
+import { IAttribute, IDeviceAttributes } from "@/types/device";
 
 import "chartjs-adapter-moment";
+import { computed } from "@vue/reactivity";
+import { date } from "quasar";
 
 export default defineComponent({
     components: { Line, DateTimePicker },
@@ -53,23 +63,26 @@ export default defineComponent({
         const data: ChartData = {
             datasets: [
                 {
-                    label: "test",
+                    label: "Sample data",
                     data: [
-                        { x: "2021-04-01", y: 86 },
-                        { x: "2021-04-02", y: 51 },
-                        { x: "2021-04-03", y: 36 },
-                        { x: "2021-04-04", y: 36 },
-                        { x: "2021-04-05", y: 26 },
-                        { x: "2021-04-06", y: 19 },
-                        { x: "2021-04-07", y: 96 },
-                        { x: "2021-04-08", y: 63 },
-                        { x: "2021-04-09", y: 85 },
-                        { x: "2021-04-10 10:25:54", y: 46 },
-                        { x: "2021-04-10 11:25:54", y: 50 },
-                        { x: "2021-04-10 13:25:54", y: 46 },
-                        { x: "2021-04-10 20:25:54", y: 48 },
-                        { x: "2021-04-10 20:25:54", y: 46 },
+                        { createdAt: "2022/01/01", value: 84 },
+                        { createdAt: "2022/02/01", value: 54 },
+                        { createdAt: "2022/03/01", value: 38 },
+                        { createdAt: "2022/04/01", value: 55 },
+                        { createdAt: "2022/05/01", value: 14 },
+                        { createdAt: "2022/06/01", value: 3 },
+                        { createdAt: "2022/07/01", value: 26 },
+                        { createdAt: "2022/08/01", value: 96 },
+                        { createdAt: "2022/09/01", value: 68 },
+                        { createdAt: "2022/10/01", value: 100 },
+                        { createdAt: "2022/11/01", value: 90 },
+                        { createdAt: "2022/12/01", value: 40 },
                     ],
+                    borderColor: "#" + Math.round(0xffffff * Math.random()).toString(16),
+                    parsing: {
+                        xAxisKey: "createdAt",
+                        yAxisKey: "value",
+                    },
                 },
             ],
         };
@@ -80,27 +93,53 @@ export default defineComponent({
             scales: {
                 x: {
                     type: "time",
-                    time: {
-                        unit: "hour",
-                    },
                 },
             },
-                elements: {
-                    point:{
-                        radius: 0
-                    }
-                }
-            
+            elements: {
+                point: {
+                    radius: 1,
+                },
+            },
         };
 
         const options = ref(opt);
 
         let date_start = ref<Date | null>(null);
         let date_end = ref<Date | null>(null);
+        let range = ref("Last day");
+        let search = ref("");
 
         let devices = ref<IDeviceAttributes[]>([]);
-
         let selected = ref<number[]>([]);
+
+        watch(
+            range,
+            (val) => {
+                console.log("Range watcher", val);
+                if (val == "Last day") {
+                    date_end.value = new Date();
+                    date_start.value = new Date();
+                    date_start.value.setDate(date_start.value.getDate() - 1);
+                }
+                if (val == "Last week") {
+                    date_end.value = new Date();
+                    date_start.value = new Date();
+                    date_start.value.setDate(date_start.value.getDate() - 7);
+                }
+                if (val == "Last month") {
+                    date_end.value = new Date();
+                    date_start.value = new Date();
+                    date_start.value.setDate(date_start.value.getDate() - 30);
+                }
+            },
+            { immediate: true }
+        );
+
+        const filtered_attributes = computed(() => {
+            return devices.value.filter((dev) => {
+                return dev.name.includes(search.value) || dev.attributes.find((a) => a.name.includes(search.value));
+            });
+        });
 
         function is_active(attr_id: number) {
             return selected.value.includes(attr_id);
@@ -121,22 +160,28 @@ export default defineComponent({
             let ids = [...selected.value];
             let start = date_start.value as Date;
             let end = date_end.value as Date;
-            const values = (await FetchTelemetry(ids, start, end)).filter((a: any) => a.value != null);
+            const attributes = await FetchTelemetry(ids, start, end);
             let i = 0;
-            chartData.value.datasets = selected.value.map((a) => {
-                let name = devices.value.find((attr) => attr.id == a);
+
+            console.log(attributes);
+
+            chartData.value.datasets = attributes.map((a: any) => {
                 return {
-                    label: name?.name,
-                    data: values.map((v: any) => {
-                        return {
-                            x: v.createdAt,
-                            y: v.value,
-                        };
-                    }),
-                    borderColor: colors[i++]
+                    label: a.name,
+                    data: a.Telemetry.filter((d: any) => d.value != null),
+                    borderColor: "#" + Math.round(0xffffff * Math.random()).toString(16),
+                    parsing: {
+                        xAxisKey: "createdAt",
+                        yAxisKey: "value",
+                    },
                 };
             });
-           // options.value!.scales!.x!.min = Math.min(...values.map((v:any)=>v.value));
+
+            if (date_start.value) {
+                options.value!.scales!.x!.min = start;
+            }
+
+            if (date_end.value) options.value!.scales!.x!.max = end;
 
             console.log(options.value);
             console.log(chartData.value);
@@ -146,7 +191,7 @@ export default defineComponent({
             devices.value = await GetDevicesAttributes();
         });
 
-        return { devices, click_attr, is_active, chartData, options, date_start, date_end, fetch_data };
+        return { devices, click_attr, is_active, chartData, options, date_start, date_end, fetch_data, range, search, filtered_attributes };
     },
 });
 </script>
